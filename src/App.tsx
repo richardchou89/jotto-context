c04da590b7ce7bcc46eee891de632bc98afe7112
Richard Chou <konami99@hotmail.com>
2025-05-28 22:32:06 +1000

    get secret word

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useReducer, useEffect } from 'react'
+import { getSecretWord } from './actions';
 import './App.css'
 
 type State = {
@@ -23,12 +24,41 @@ const reducer = (state: State, action: Action) => {
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(
+    reducer,
+    { secretWord: '', language: 'en' } as State
+  )
 
+  const success = false;
+  const guessedWords = [];
+
+  const setSecretWord = (secretWord: string) => {
+    dispatch({ type: 'setSecretWord', payload: secretWord })
+  }
+  const setLanguage = (language: string) => {
+    dispatch({ type: 'setLanguage', payload: language })
+  }
+
+  useEffect(() => {
+    getSecretWord(setSecretWord)
+  }, []);
+
+  if (state.secretWord === null) {
+    return (
+      <div className="container" data-test="spinner">
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <p>Loading secret word...</p>
+      </div>
+    )
+  }
+  
   return (
-    <>
-      
-    </>
+    <div data-test="component-app" className="container">
+      <h1>Jotto</h1>
+      <p>The secret word is {state.secretWord}</p>
+    </div>
   )
 }
 
